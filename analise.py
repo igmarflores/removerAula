a8127e6bb2f130ed1e1b44d29f1ff1d12ae44068
igmarflores <igmarvin@hotmail.com>
2023-05-22 14:30:28 -0400

    [feat]:adicionndo informacoes sobre o resquisito

---
+++ analise.py
@@ -0,0 +1,3 @@
+#tomar em conta todas as atividades que consideram modelagem matematica
+#considerar empresas que tenham dados sobre analise de risco
+#nao ha empresas com expertisie em gis e cadastro de drenagem
